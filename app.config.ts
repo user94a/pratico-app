d812f3f4efbc3d9cf1a12d2afd53ff207e9565ea
user94a <calamandrei@extendi.it>
2025-08-21 12:39:19 +0200

    Aggiorna numero build

--- app.config.ts
+++ app.config.ts
@@ -19,7 +19,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ios: {
     supportsTablet: true,
     bundleIdentifier: 'it.extendi.pratico',
-    buildNumber: '6',
+    buildNumber: '7',
     userInterfaceStyle: 'light',
     infoPlist: {
       NSCameraUsageDescription: 'Questa app usa la fotocamera per scattare foto dei documenti da allegare.',
